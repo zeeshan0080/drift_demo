3df3790dce7dbb4cdafcb63c3b714e68c3a5cdeb
zeeshan0080 <zeeshan_breseli@hotmail.com>
2024-11-21 19:37:35 +0500

    CRUD queries added

--- lib/source/core/local_client.dart
+++ lib/source/core/local_client.dart
@@ -13,9 +13,15 @@ class LocalClient{
     return users.map((item) => UserModel.fromLocal(item)).toList();
   }
 
+  Future<UserModel?> getUserByID({required int id}) async {
+    final response = await (db.select(db.userLocalModel)..where((tbl) => tbl.id.equals(id))).getSingleOrNull();
+    if(response == null) return null;
+    return UserModel.fromLocal(response);
+  }
+
   Future<int> addUser({required UserModel userDetails}) async {
     final response = await db.into(db.userLocalModel).insert(
-      mode: InsertMode.insertOrFail,
+      mode: InsertMode.insertOrReplace,
         UserLocalModelCompanion.insert(
           firstName: Value(userDetails.firstName),
           lastName: Value(userDetails.lastName),
@@ -25,5 +31,30 @@ class LocalClient{
       )
     );
     return response;
+  }
+
+  Future<void> updateUserDetails({required UserModel userDetails}) async {
+    final data = UserLocalModelCompanion(
+      firstName: Value(userDetails.firstName),
+      lastName: Value(userDetails.lastName),
+      email: Value(userDetails.email),
+      age: Value(userDetails.age),
+      isVerified: Value(userDetails.isVerified??false),
+    );
+    final response = await (db.update(db.userLocalModel)..where((tbl) => tbl.id.equals(userDetails.id!))).write(data);
+    print("User record updated: $response");
+    return;
+  }
+
+  Future<void> deleteUserByID({required int id}) async {
+    final count = await (db.delete(db.userLocalModel)..where((tbl) => tbl.id.equals(id))).go();
+    print("User records deleted: $count");
+    return;
+  }
+
+  Future<void> deleteAllUsers() async {
+    final count = await db.delete(db.userLocalModel).go();
+    print("All Users deleted: $count");
+    return;
   }
 }
